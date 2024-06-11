c504c6efd1cdd4361156ea3f328ff52aac21ead1
Kahfi Aditia <67872471+kahfiaditia@users.noreply.github.com>
2024-06-11 13:42:06 +0700

    data

--- database/seeders/KategoriSeeder.php
+++ database/seeders/KategoriSeeder.php
@@ -24,73 +24,55 @@ class KategoriSeeder extends Seeder
                 'nama' => 'PENDIDIKAN',
                 'deskripsi' => 'PENDIDIKAN',
                 'aktif' => 1,
-                'user_created' => 1,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
+
             ],
             [
                 'nama' => 'SOSIAL',
                 'deskripsi' => 'SOSIAL',
                 'aktif' => 1,
-                'user_created' => 1,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
+
             ],
             [
                 'nama' => 'POLITIK',
                 'deskripsi' => 'POLITIK',
                 'aktif' => 1,
-                'user_created' => 1,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
+
             ],
             [
                 'nama' => 'ARTIKEL',
                 'deskripsi' => 'ARTIKEL',
                 'aktif' => 1,
-                'user_created' => 1,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
+
             ],
             [
                 'nama' => 'BLOG',
                 'deskripsi' => 'BLOG',
                 'aktif' => 1,
-                'user_created' => 1,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
+
             ],
             [
                 'nama' => 'BERITA',
                 'deskripsi' => 'BERITA',
                 'aktif' => 1,
-                'user_created' => 1,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
+
             ],
             [
                 'nama' => 'CERPEN',
                 'deskripsi' => 'CERPEN',
                 'aktif' => 1,
-                'user_created' => 1,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
+
             ],
             [
                 'nama' => 'PUISI',
                 'deskripsi' => 'PUISI',
                 'aktif' => 1,
-                'user_created' => 1,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
+
             ],
             [
                 'nama' => 'PANTUN',
                 'deskripsi' => 'PANTUN',
                 'aktif' => 1,
-                'user_created' => 1,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
+
             ],
         ];
 
